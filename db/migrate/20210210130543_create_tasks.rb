c1541174d99ead2a30405fcbcc6cf2962af048a0
ryuuki.toku <ryuki109@icolud.com>
2021-02-14 05:41:22 +0000

    202102141441

--- db/migrate/20210210130543_create_tasks.rb
+++ db/migrate/20210210130543_create_tasks.rb
@@ -1,7 +1,7 @@
 class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
-      t.string :content, :status
+      t.string :content, 
 
       t.timestamps
     end
