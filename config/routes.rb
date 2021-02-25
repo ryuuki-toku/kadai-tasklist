a26c0ca607b652a6773146ba30017dd2736e643f
ryuuki.toku <ryuki109@icolud.com>
2021-02-25 22:15:00 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-   root to: 'tasks#index'
-   
-    resources :tasks
+  root to: 'tasks#index'
+  
+  resources :users, only: [:index, :show, :create, :new]
 end
-
