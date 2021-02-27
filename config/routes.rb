708a6f6617a5e00940499a402e3b30f41b70e619
ryuuki.toku <ryuki109@icolud.com>
2021-02-27 04:00:52 +0000

    post

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
+ 
   root to: 'tasks#index'
   
   get 'login', to: 'sessions#new'
@@ -10,4 +8,6 @@ Rails.application.routes.draw do
   
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :create,]
+  
+  resources :tasks, only: [:index, :create, :destroy]
 end
